b0edfbee57091bd4ced5d24c00648b2c549f5299
MuhamadJunaidHunani <junaidhunani890@gmail.com>
2025-01-26 08:19:27 +0500

    oksh

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+    minify: 'terser', // Use Terser minifier to control warnings
+    chunkSizeWarningLimit: 1000, // Increase the chunk size limit to avoid the warning
+  },
 })
