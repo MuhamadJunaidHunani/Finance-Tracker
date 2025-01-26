eff4fc16c4fec346a44de5de67fa481c84a94a9e
MuhamadJunaidHunani <junaidhunani890@gmail.com>
2025-01-26 08:30:03 +0500

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          // Split node_modules into separate chunks
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
+        // Manual chunks to separate dependencies into smaller bundles
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // React aur React DOM ko alag chunk mein rakhna
         },
       },
     },
-    chunkSizeWarningLimit: 2000, // Increase chunk size limit to avoid warnings
+    chunkSizeWarningLimit: 1000, // Chunk size limit ko 1000 kB tak adjust kar diya
   },
-})
+});
