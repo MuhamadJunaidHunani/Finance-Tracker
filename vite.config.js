619221625b78e95aa7b586b30b8e6b2082335a19
MuhamadJunaidHunani <junaidhunani890@gmail.com>
2025-01-26 08:22:06 +0500

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    terserOptions: {
-      compress: {
-        drop_console: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split node_modules into separate chunks
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
       },
     },
-    minify: 'terser', // Use Terser minifier to control warnings
-    chunkSizeWarningLimit: 1000, // Increase the chunk size limit to avoid the warning
+    chunkSizeWarningLimit: 2000, // Increase chunk size limit to avoid warnings
   },
 })
