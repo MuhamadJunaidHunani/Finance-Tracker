938d3a1222801146aa95935821c150a9394fbf71
MuhamadJunaidHunani <junaidhunani890@gmail.com>
2025-01-26 08:39:19 +0500

    ok2

--- src/Components/UI/Loader.jsx
+++ src/Components/UI/Loader.jsx
@@ -1,6 +1,4 @@
-import Lottie from 'lottie-react'
 import React from 'react'
-import loaderAnimation from "../../assets/JsonIcon/wired-lineal-298-coins-hover-jump.json";
 import { FiBarChart } from "react-icons/fi";
 
 const Loader = () => {
@@ -8,12 +6,6 @@ const Loader = () => {
     <div className="fixed top-0 left-0 h-screen w-screen flex items-center justify-center bg-[#F7F5F3]">
       <div className='flex flex-col items-center justify-center'>
         <div className="flex justify-center items-center gap-2">
-          <Lottie
-            animationData={loaderAnimation}
-            loop={true}
-            style={{ width: 60, height: 60 }}
-          />
-          
           <p className='text-[21px] text-[#303030] font-semibold'>Quick Token</p>
         </div>
         <div className="w-[200px] h-[2px] bg-[#e4e4e4] overflow-hidden relative">
@@ -21,7 +13,7 @@ const Loader = () => {
         </div>
         <div className="mt-[20px] flex gap-1">
           <p className='text-[12px] text-[#959595]'>Your Token Tracker</p>
-          <FiBarChart className='text-[#959595]'/>
+          <FiBarChart className='text-[#959595]' />
         </div>
       </div>
 
