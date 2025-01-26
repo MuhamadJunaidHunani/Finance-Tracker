54e4e327ba576e60c5d681a64bbf36722fadf1cb
MuhamadJunaidHunani <junaidhunani890@gmail.com>
2025-01-26 08:10:59 +0500

    okh

--- Config.js
+++ Config.js
@@ -1,5 +1,5 @@
 // const BaseUrl = "https://coincraft-backend.vercel.app/api" //production
-const BaseUrl = "https://hackathon-backend-khaki.vercel.app/api" //production
+const BaseUrl = "https://quick-token-backend-uqfb.vercel.app/api" //production
 // const BaseUrl = "http://localhost:1234/api" //local
 
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Hero from '../Components/Sections/Hero'
 import Header from '../Components/Layouts/Header'
-import ConverterIntro from '../Components/Sections/converterIntro'
-import OurClients from '../Components/Sections/OurClients'
 import Footer from '../Components/Layouts/Footer'
 
 
